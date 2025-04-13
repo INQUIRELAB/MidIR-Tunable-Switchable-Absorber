5c2b93e8e6ebe510c8d9b765789cd9a802583db0
INQUIRELAB <bana@ou.edu>
2025-04-13 11:23:35 -0500

    Add files via upload

--- data.m
+++ data.m
@@ -2,22 +2,23 @@
 % raw_data_script.m
 % -----------------------------------------------------------------------
 % This MATLAB script releases and documents the dataset used in the paper:
-% [].
+% [Inverse designed aperiodic multilayer perfect absorbers for mid infrared
+% enable tunability switchability and angular robustness].
 % The dataset is provided to ensure reproducibility of the results and to
 % allow other researchers to explore the data.
 %
-% Author: []
+% Author: [Masoumeh Nazari, Yaser M. Banad, Sarah Sharif]
 % Date: [April 11, 2025]
-% Contact: []
-% Citation:
-% If you use this dataset in your work, please cite the following paper:
-% [].  
-% DOI:
+% Contact: [Corresponding Author: s.sh@ou.edu]
+% License:
+% This project and associated raw data are licensed under the MIT License.  
 % -----------------------------------------------------------------------
 
 
-%%Figure 1
-%% Raw data corresponding to Figure 3(a) in the paper. This plot presents the absorption spectra of all optimized structures(a-i) in chemical potentia =0eV and incident Angle of 0 degree. 
+%% Figure 1
+% Raw data corresponding to Figure 3(a) in the paper. 
+% This plot presents the absorption spectra of all optimized structures
+% (a-i) in chemical potentia =0eV and incident Angle of 0 degree. 
 figure(1)
 set(gcf,'color','w');
 matrixoutput=csvread('absorption_data_3um_0_0.csv');
@@ -69,11 +70,14 @@ xlabel ('wavelength(um)','FontSize',16)
 ylabel ('Absorption','FontSize',16)
 axis([2.5 6 0 1])
 legend('a','b', 'c','d','e','f','g','h','i','fontsize',24);
-%%--------------------------------------------------------------------------------------------------------------------------------------------------------
-%%Figure2
-%% Raw data related to Figure 7 of the paper. 
-%% While the figure in the paper is a 3D plot, this graph shows the 2D version. 
-%% It displays the absorption spectra of all optimized structures (a–i) at a chemical potential of 0 eV and an incident angle of 45 degrees.
+
+%%-------------------------------------------------------------------------
+%% Figure2
+% Raw data related to Figure 7 of the paper. 
+% While the figure in the paper is a 3D plot, this graph shows the 
+% 2D version. 
+% It displays the absorption spectra of all optimized structures (a–i) 
+% at a chemical potential of 0 eV and an incident angle of 45 degrees.
 figure(2)
 set(gcf,'color','w');
 matrixoutput=csvread('absorption_data_3um_0_45.csv');
@@ -125,11 +129,12 @@ xlabel ('wavelength(um)','FontSize',16)
 ylabel ('Absorption','FontSize',16)
 axis([2.5 6 0 1])
 legend('a','b', 'c','d','e','f','g','h','i','fontsize',24);
-%%--------------------------------------------------------------------------------------------------------------------------------------------------------
-%%Figure 3
-%% Raw data related to Figure 5 of the paper. 
-%% While the figure in the paper is a 3D plot, this graph shows the 2D version. 
-%% It displays the absorption spectra of all optimized structures (a–i) at a chemical potential of 1 eV and an incident angle of 0 degrees.
+%%-------------------------------------------------------------------------
+%% Figure 3
+% Raw data related to Figure 5 of the paper. 
+% While the figure in the paper is a 3D plot, this graph shows the 2D version. 
+% It displays the absorption spectra of all optimized structures (a–i) at 
+% a chemical potential of 1 eV and an incident angle of 0 degrees.
 figure(3)
 set(gcf,'color','w');
 matrixoutput=csvread('absorption_data_3um_1_0.csv');
